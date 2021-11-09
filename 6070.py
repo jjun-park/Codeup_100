297da48ff85986b54ab9b87f9bc6a167e60f8a13
jjun-park <tkdldjs2654@naver.com>
2021-11-09 21:07:50 +0900

    "to increase my SKILL!!!"

---
+++ 6070.py
@@ -0,0 +1,11 @@
+n = input()
+n = int(n)
+
+if n // 3 == 1:
+    print("spring")
+elif n // 3 == 2:
+    print("summer")
+elif n // 3 == 3:
+    print("fall")
+elif n // 3 == 4 or n // 3 == 0:
+    print("winter")
