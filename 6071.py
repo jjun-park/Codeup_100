aa5cc67953aeed090c25359946a9a1a742cfd9e6
jjun-park <tkdldjs2654@naver.com>
2021-11-10 21:08:51 +0900

    "Hell... Always HELL!!!"

---
+++ 6071.py
@@ -0,0 +1,7 @@
+c = 5
+while c:
+    c = int(input())
+    if c != 0:
+        print(c)
+    else:
+        break
---
+++ 6074.py
@@ -0,0 +1,5 @@
+z = ord(input())
+x = ord('a')
+while x <= z:
+    print(chr(x), end=' ')
+    x += 1
