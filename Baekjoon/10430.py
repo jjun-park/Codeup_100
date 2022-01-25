66b5158a58a2f00dd80252fda6f23f039d8a47de
jjun-park <tkdldjs2654@naver.com>
2022-01-25 21:09:55 +0900

    "Restart!"

---
+++ Baekjoon/10430.py
@@ -0,0 +1,8 @@
+from sys import stdin
+input = stdin.readline
+
+A, B, C = map(int, input().split())
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
---
+++ Baekjoon/2420.py
@@ -0,0 +1,5 @@
+from sys import stdin
+input = stdin.readline
+
+N, M = map(int, input().split())
+print(abs(N - M))
