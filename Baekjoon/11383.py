90c5f23cb77d96507fd73fe2ab7f3d60de678a48
jjun-park <tkdldjs2654@naver.com>
2022-03-02 22:15:39 +0900

    "Q"

---
+++ Baekjoon/11383.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+firImg = [input() for _ in range(N)]        # 돌돔 사진     # ABCDE
+secImg = [input() for _ in range(N)]        # 됾 사진      # AABBCCDDEE
+
+Check = True
+
+for i in range(N):
+    ref = ""        # 검증 기준이 되는 리스트 값을 받기 위해 공백으로 설정함
+    for j in firImg[i]:     # firImg의 0번째 값: ABCDE -> 반복문 돌리게 되면 리스트값이 A, B, C, D, E로 쪼개져 들어감
+        ref += j * 2
+
+    if ref != secImg[i]:    # secImg 리스트의 0번째 값: AABBCCDDEE
+        checker = False
+        break
+
+print("Eyfa" if Check else "Not Eyfa")      # True값 "if" 조건문 "else" False값
